04519c6d27795a21b3f60a4a5c1b5203d0062840
dhuszti <huszti.daniel@gmail.com>
2016-10-19 12:08:15 +0100

    NER extraction to dictionaries

--- src/NER.py
+++ src/NER.py
@@ -3,8 +3,9 @@
 
 import os, sys, getopt, csv
 from polyglot.text import Text
+from itertools import chain
 
-# This function creates 3 dictionaries for location, person and organization. Important, both ones are in UNICODE encoding! 
+# This function creates 3 dictionaries for location, person and organization. Important, every result set is in UNICODE encoding! 
 def NER_Dictionary(CorpusFilePath):
 	corpusfile = open(CorpusFilePath, 'rb')
 	csvreader = csv.reader(corpusfile, delimiter='\t')
@@ -13,26 +14,35 @@ def NER_Dictionary(CorpusFilePath):
 	personList = []
 	organizationList = []
 
-	tempList = []
-
 	for line in csvreader:
 		try:
 			blob = str(line[4]).decode('latin2')
 			text = Text(blob)
 			for sent in text.sentences:
 				for entity in sent.entities:
-					if entity not in tempList:
-				    		if 'PER' in entity.tag:
-							personList.extend(entity)
-						elif 'LOC' in entity.tag:
-							locationList.extend(entity)
-						elif 'ORG' in entity.tag:
-							organizationList.extend(entity)
-
-					tempList.extend(entity)
+					tag = entity.tag
+					for element in entity:
+						if element not in chain(personList, locationList, organizationList):
+					    		if 'PER' in tag:
+								personList.append(element)
+							elif 'LOC' in tag:
+								locationList.append(element)
+							elif 'ORG' in tag:
+								organizationList.append(element)
 		except:
 			pass
 	
+	# Filter out some mistaken nouns
+	person = []
+	for word in personList:
+    		if word[0].isupper():
+        		person.append(word)
+
+	# Sort them to make it more understandable for human beings
+	locationList = sorted(locationList)
+	personList = sorted(person)
+	organizationList = sorted(organizationList)	
+
 	return (locationList, personList, organizationList)
 	
 
@@ -40,7 +50,7 @@ def main():
 	PreprocessedCorpusPath='/home/osboxes/NLPtools/SentAnalysisHUN-master/OpinHuBank_20130106_new.csv'
 	(locationList, personList, organizationList) = NER_Dictionary(PreprocessedCorpusPath)
 
-	print locationList
+	print personList
 
 
 if __name__ == "__main__":
